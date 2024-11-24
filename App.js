4245844d169462f5c612ad49a4b26cac54efa876
Breachfix <Ntihanirahojacques@gmail.com>
2024-11-24 10:09:22 -0800

    Add Navbar and Home page

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
+// App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
+import Home from './src/pages/Home';
+import About from './src/pages/About';
+import Contact from './src/pages/Contact';
+import Navbar from './src/components/Navbar';
 
 const Stack = createStackNavigator();
-
-const HomeScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Welcome to WeatherAppNative!</Text>
-  </View>
-);
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Navbar />
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Contact" component={Contact} />
       </Stack.Navigator>
     </NavigationContainer>
   );
