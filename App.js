c0db5c9dade6597b6870880371cb99d6bfb06cac
Breachfix <Ntihanirahojacques@gmail.com>
2024-11-24 10:11:43 -0800

    Add Footer component

--- App.js
+++ App.js
@@ -2,22 +2,33 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { View, StyleSheet } from 'react-native';
 import Home from './src/pages/Home';
 import About from './src/pages/About';
 import Contact from './src/pages/Contact';
 import Navbar from './src/components/Navbar';
+import Footer from './src/components/Footer';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Navbar />
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="About" component={About} />
-        <Stack.Screen name="Contact" component={Contact} />
-      </Stack.Navigator>
+      <View style={styles.container}>
+        <Navbar />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="About" component={About} />
+          <Stack.Screen name="Contact" component={Contact} />
+        </Stack.Navigator>
+        <Footer />
+      </View>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
